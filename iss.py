00b69a9ca8edc1fed1095a88bf1398cbdf7006a5
Julita Marshall <julitamcg5@gmail.com>
2020-03-20 19:44:18 -0500

    not complete

--- iss.py
+++ iss.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python
 
-__author__ = '???'
+import requests
+import turtle
+import time
 
+__author__ = 'Julita Marshall'
 
+def get_astronauts():
+
+def get_iss_coordinates():
+
+def draw_iss(coords):
+
+    
 def main():
     pass
 
